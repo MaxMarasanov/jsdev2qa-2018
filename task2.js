6b0041ac40667444c9f45f1d0c2a8fa5cb515297
MaxMarasanov <maksim.marasanau@itechart-group.com>
2018-05-04 15:24:56 +0300

    Add files via upload

--- task2.js
+++ task2.js
@@ -1,36 +1,36 @@
 var arrObjects = [];
  
 arrObjects[0] = {
-    id: 0,
-    name: "Sergey"
+	id: 0,
+	name: "Sergey"
 };
 
 arrObjects[1] = {
-    id: 1,
-    name: "Max",
+	id: 1,
+	name: "Max",
 	lastName: "Ivanov"
 };
 
 arrObjects[2] = {
-    id: 2,
-    name: "Vasya",
+	id: 2,
+	name: "Vasya",
 	lastName: "Pupkin",
 	position: "Winner"
 };
 
 arrObjects[3] = {
-    id: 3,
-    name: "Lena",
+	id: 3,
+	name: "Lena",
 };
 
 arrObjects[4] = {
-    id: 4,
+	id: 4,
 };
 
 //условие сортировки - по убыванию количества свойст
 function compareLength(obj1, obj2) 
 {
-  return Object.keys(obj2).length - Object.keys(obj1).length
+	return Object.keys(obj2).length - Object.keys(obj1).length
 }
 
 //вывожу уже отсортированный массив объектов

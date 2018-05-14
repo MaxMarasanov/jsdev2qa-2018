9bac3b1d3955fcc2a50be225930ddbc366077790
MaxMarasanov <maksim.marasanau@itechart-group.com>
2018-05-14 11:17:56 +0300

    use strict equality in array search
    Strict equality for if condition in task3 function

--- task3.js
+++ task3.js
@@ -5,7 +5,7 @@ function task3()
 	console.log("Массив до: " + fruits);
 	for (var i = 0; i < fruits.length; i++) 
 	{ 
-		if (fruits[i] == "Манго")
+		if (fruits[i] === "Манго")
 		fruits.splice(i, 1); //я использую splice, а не delete, чтобы не оставлять undefined значений
 	} 
 	console.log("Массив после: " + fruits);

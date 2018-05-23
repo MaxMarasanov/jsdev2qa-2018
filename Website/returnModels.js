5b7960e498dcd423d8ce78011c2cfc17bad664e4
MaxMarasanov <maksim.marasanau@itechart-group.com>
2018-05-23 16:52:28 +0300

    bug fixes in 1st BE method
    - renamed manufacturerSelectedValue variable to manufacturerValue;
    - moved declaration of jsonModels outside of if else constructions in exports.models function;
    - renamed variable for returnModels module declaration;
    - added declaration of external modules variables;
    - fixed formatting of returnModules.js

--- Website/returnModels.js
+++ Website/returnModels.js
@@ -3,33 +3,35 @@ var iphone = ["iPhone 6s", "iPhone 7", "iPhone 7 Plus", "iPhone 8", "iPhone 8 Pl
 var samsung = ["Samsung Galaxy S6", "Samsung Galaxy S8", "Samsung Galaxy S7", "Samsung Galaxy S9+"];
 var nokia = ["Nokia 3310", "Nokia Lumia 850", "Nokia X", "Nokia XL"];
 
-exports.models = function(manufacturerSelectedValue) {
+exports.models = function(manufacturerValue) {
 	
-	if (manufacturer.indexOf(manufacturerSelectedValue) !== -1) {
-		var jsonModels = JSON.stringify(eval(manufacturerSelectedValue));
+	var jsonModels = "";
+	
+	if (manufacturer.indexOf(manufacturerValue) !== -1) {
+		jsonModels = JSON.stringify(eval(manufacturerValue));
 	}
 	else {
 		var emptyArray = [""];
-		var jsonModels = JSON.stringify(emptyArray);
+		jsonModels = JSON.stringify(emptyArray);
 	}
 	return jsonModels;
 };
 
 var http = require('http');
-var dt = require('./returnModels');
-formidable = require('formidable'),
-util = require('util');
+var returnModels = require('./returnModels');
+var formidable = require('formidable'),
+var util = require('util');
 
 http.createServer(function (request, response) {
 	if (request.method == 'POST') {
 		var form = new formidable.IncomingForm();
 		form.parse(request, function(err, fields, files) {
-			var manufacturerSelectedValue = fields.manufacturer;
+			var manufacturerValue = fields.manufacturer;
 			response.setHeader('Access-Control-Allow-Origin', '*');
 			response.writeHead(200, {'Content-Type': 'text/html'});	
-			response.write(dt.models(manufacturerSelectedValue));
+			response.write(returnModels.models(manufacturerValue));
 			response.end();
-    	});	
+		});	
 	}
 	else {
 		response.setHeader('Access-Control-Allow-Origin', '*');
